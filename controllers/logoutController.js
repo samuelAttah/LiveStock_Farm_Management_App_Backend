df1d004337e90d7545f93bf2cdcb73753afaa842
Samuel Attah <sammy.attah16@gmail.com>
2022-09-14 20:23:42 +0100

    updated logout controller and route

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -5,32 +5,17 @@ const handleLogout = asyncHandler(async (req, res) => {
   //on client, also delete the accessToken
 
   const cookies = req.cookies;
-  if (!cookies?.jwt) return res.sendStatus(204); //204 here means we don't have cookies which means we are successful
-  const refreshToken = cookies.jwt;
+  if (!cookies?.jwt) return res.status(204).json({ message: "Cookie Cleared" }); //204 here means we don't have cookies which means we are successful
 
-  //Is refresh token in database?
-  const foundUser = await User.findOne({ refreshToken: refreshToken }).exec();
-  if (!foundUser) {
-    res.clearCookie("jwt", {
+  //Delete the refreshToken in db, by setting the refresh token back to an empty string and saving it back to our database
+  return res
+    .clearCookie("jwt", {
       httpOnly: true,
       sameSite: "None",
       secure: true,
-    });
-    return res.sendStatus(204);
-  }
-
-  //Delete the refreshToken in db, by setting the refresh token back to an empty string and saving it back to our database
-  foundUser.refreshToken = foundUser.refreshToken.filter(
-    (rt) => rt !== refreshToken
-  );
-  const result = await foundUser.save();
-  console.log("result", result);
-  res.clearCookie("jwt", {
-    httpOnly: true,
-    sameSite: "None",
-    secure: true,
-  });
-  res.sendStatus(204);
+    })
+    .status(204)
+    .json({ message: "Cookie Cleared" });
 });
 
 module.exports = { handleLogout };
