ddaa4c3f18d2be2c9c64566b993375cb692a8362
Samuel Attah <sammy.attah16@gmail.com>
2022-10-23 15:12:52 +0100

    ready to deploy backend

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,9 +3,9 @@
 const allowedOrigins = require("./allowedOrigins");
 const corsOptions = {
   //the following checks if the origin is part of the allowedOrigins.
-  //NOTE || !origin below avoids blocking REST tools or server to server requests and should be used majorly in development.
+  //NOTE || !origin below avoids blocking REST tools or server to server requests without origin like postman.
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
