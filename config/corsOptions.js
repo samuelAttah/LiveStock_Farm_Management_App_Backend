db92b705a349ed4b4646e74bcf97d7e1cdcbe588
Samuel Attah <sammy.attah16@gmail.com>
2022-10-23 15:31:34 +0100

    Made some Changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,7 +5,7 @@ const corsOptions = {
   //the following checks if the origin is part of the allowedOrigins.
   //NOTE || !origin below avoids blocking REST tools or server to server requests without origin like postman.
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
